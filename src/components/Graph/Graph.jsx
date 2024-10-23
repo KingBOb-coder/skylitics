a25519660e1e275bb7f356e9ab65ce28eb225812
KingBOb-coder <dheemanthakar443@gmail.com>
2024-10-23 12:10:52 +1100

    update

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Graph.css";
 
 const Graph = () => {
-  return <div>Graph hellos hello</div>;
+  return <div>Graph helloss hello</div>;
 };
 
 export default Graph;
